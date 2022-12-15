0b4c1ae4100c26c029c215ca6aac96a24e61e00a
SKylinka <96301666+SKylinka@users.noreply.github.com>
2022-12-15 12:20:58 +0300

    15.12

--- fns-zdp/src/test/java/ru/stqa/pft/idsys/tests/DeleteDocTest.java
+++ fns-zdp/src/test/java/ru/stqa/pft/idsys/tests/DeleteDocTest.java
@@ -1,13 +1,17 @@
-package ru.stqa.pft.idsys.tests;
+package com.example;
 
 import java.time.Duration;
-
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
 import static org.testng.Assert.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import java.io.File;
 
-public class NewCreationTest {
+
+public class DeleteDocTest {
   private WebDriver driver;
   private String baseUrl;
   private boolean acceptNextAlert = true;
@@ -24,7 +28,7 @@ public class NewCreationTest {
   }
 
   @Test
-  public void testDocCreation() throws Exception {
+  public void testDeleteDoc() throws Exception {
     driver.get("http://192.168.1.222:8080/bank_client/");
     driver.findElement(By.id("LOGIN-USERNAME")).click();
     driver.findElement(By.id("LOGIN-USERNAME")).clear();
@@ -33,25 +37,18 @@ public class NewCreationTest {
     driver.findElement(By.id("LOGIN-PASSWORD")).clear();
     driver.findElement(By.id("LOGIN-PASSWORD")).sendKeys("123456");
     driver.findElement(By.id("BTN-OK")).click();
-    driver.findElement(By.xpath("//*[@id=\"bankclient-538598663\"]/div/div[2]/div/div[2]/div/div/div[1]/div/div/div/div[1]/div/div[1]/div")).click();
-    driver.findElement(By.xpath("//*[@id=\"gwt-uid-12\"]")).click();
-    driver.findElement(By.xpath("//*[@id=\"bankclient-538598663\"]/div/div[2]/div/div[2]/div/div/div[3]/div/div[2]/div/div/div[2]/div/div/div/div/div/div[1]/div/div[2]")).click();
-    //driver.findElement(By.xpath("//*[@id=\"REF-BANK_BIK43002413\"]")).click();
-    //driver.findElement(By.xpath("//div[@id='bankclient-538598663']/div/div[2]/div/div[2]/div/div/div[3]/div/div[2]/div/div/div[2]/div/div/div/div/div/div[2]/div/div[2]/div/div[2]/div/table/tbody/tr/td[2]/div")).click();
-    //driver.findElement(By.xpath("//div[@id='bankclient-538598663']/div/div[2]/div/div[2]/div/div/div[3]/div/div[2]/div/div/div[2]/div/div/div/div/div/div[2]/div/div[2]/div/div[2]/div/table/tbody/tr/td[2]/div")).click();
-    //driver.findElement(By.xpath("//div[@id='bankclient-538598663']/div/div[2]/div/div[2]/div/div/div[3]/div/div[2]/div/div/div[2]/div/div/div/div/div/div[2]/div/div[2]/div/div[2]/div/table/tbody/tr/td[2]/div")).click();
 
+    driver.findElement(By.xpath("//div[@id='bankclient-538598663']/div/div[2]/div/div[2]/div/div/div/div/div/div/div/div/div/div")).click();
 
-    driver.findElement(By.xpath("//*[contains(@class, 'v-textfield v-widget')]")).click();
-    driver.findElement(By.xpath("//*[contains(@class, 'v-textfield v-widget')]")).clear();
-    //driver.findElement(By.xpath("//*[@id=\"bankclient-538598663\"]/div/div[2]/div/div[2]/div/div/div[3]/div/div[2]/div/div/div[2]/div/div/div/div/div/div/div/div[2]/div/div/div/table/tbody/tr[3]/td[3]/div/div[2]/div/div")).clear();
-    driver.findElement(By.xpath("//*[contains(@class, 'v-textfield v-widget')]")).sendKeys("123456789000");
+    driver.findElement(By.xpath("//div[@id='gwt-uid-12']/span")).click();
 
+    driver.findElement(By.xpath("//div[@id='bankclient-538598663']/div/div[2]/div/div[2]/div/div/div[3]/div/div[2]/div/div/div[2]/div/div/div/div/div/div[3]/div/div[2]/div/div[2]/div/table/tbody/tr/td/div")).click();
 
-    driver.findElement(By.xpath("//*[@id=\"bankclient-538598663\"]/div/div[2]/div/div[2]/div/div/div[3]/div/div[2]/div/div/div[3]/div/div/div/div/div[2]/div/div[3]")).click();
-    /*
-    */
+    driver.findElement(By.xpath("//*[@id=\"bankclient-538598663\"]/div/div[2]/div/div[2]/div/div/div[3]/div/div[2]/div/div/div[2]/div/div/div/div/div/div[1]/div/div[5]")).click();
 
+    driver.findElement(By.xpath("//*[contains(@class, 'v-button v-widget v-pressed')]")).click();
+
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
   }
 
   @AfterClass(alwaysRun = true)
---
+++ fns-zdp/src/test/java/ru/stqa/pft/idsys/tests/ZdpData.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.idsys.tests;
+
+public class ZdpData {
+
+  // Класс для инициализации объектов и атрибутов для полей запроса
+  private final String inn;
+
+  public ZdpData(String inn) {
+    this.inn = inn;
+  }
+
+  public String getInn() {
+    return inn;
+  }
+}
