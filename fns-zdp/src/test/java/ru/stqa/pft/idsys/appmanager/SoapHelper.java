6fcba3a35320ec2b3d0fd38498083f0e65ccea39
SKylinka <96301666+SKylinka@users.noreply.github.com>
2023-01-26 17:55:40 +0300

    26.01

--- fns-zdp/src/test/java/ru/stqa/pft/idsys/appmanager/SoapHelper.java
+++ fns-zdp/src/test/java/ru/stqa/pft/idsys/appmanager/SoapHelper.java
@@ -54,63 +54,6 @@ public class SoapHelper implements SOAPHandler<SOAPMessageContext> {
    }
 
 
-/**
-  public void createLookupCustomersRqOld(LookupCustomersRqData lookupCustomersRqData) throws LookupCustomersErr {
-    LookupCustomersRq getLookupCustomersRq = new LookupCustomersRq();
-    getLookupCustomersRq.setDataFilter( new CustomerDataFilterItem() {{
-      setTimeFilter(lookupCustomersRqData.withCustomerDataType());
-    }});
-    getLookupCustomersRq.setCustomers( new Customer() {{
-      setINN(lookupCustomersRqData.withInn());
-    }});
-
-    LookupCustomersRs getlookupCustomersRs = getCustomersPort().lookupCustomers(getLookupCustomersRq);
-
-  }
- */
-
-// WS-Security header values
-public static final String SECURITY = "Security";
-  public static final String USERNAME_TOKEN = "UsernameToken";
-  public static final String USERNAME = "Username";
-  public static final String PASSWORD = "Password";
-  public static final String WS_SECURITY_NAMESPACE =
-          "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd";
-  public static final String WS_SECURITY_NAMESPACE2 =
-          "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd";
-  public static final String WS_SECURITY_NAMESPACE3 =
-          "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText";
-
-  // prefixes
-  public static final String WSSE_PREFIX = "wsse"; // ws service security
-  public static final String SPSS_PREFIX = "spss"; // spss prefix
-
-  // SPSS custom language header values
-  public static final String SPSS_HEADER_NAMESPACE = "http://xml.spss.com/ws/headers";
-  public static final String CLIENT_ACCEPT_LANGUAGE_HEADER = "client-accept-language";
-
-  private String i_username;
-  private String i_password;
-  private String i_acceptLanguage;
-
-  /**
-   * Creates a security and language handler
-   * @param username A user name to access the web service. Cannot be null.
-   * @param password A password to access the web service. Cannot be null.
-   * @param acceptLanguage The language that should be used by the web service.
- * This value should be formatted according to the HTTP specification regarding
- * the Accept-Language HTTP header (e.g. en-US;q=1.0, en;q=0.8)
-   */
-  public void SecurityHandler(String username, String password, String acceptLanguage)
-  {
-    i_username = properties.getProperty("soap.Login");
-    i_password = properties.getProperty("soap.Password");
-    i_acceptLanguage = null;
-  }
-
-
-
-
   public boolean handleMessage(SOAPMessageContext context)
   {
     // Apply this handler to only outbound traffic
@@ -127,14 +70,23 @@ public static final String SECURITY = "Security";
         {
           header = envelope.addHeader();
         }
-
         // add the UsernameToken header
-        header.addChildElement(createUsernameTokenSecurityHeader());
-        // assuming the language was provided, apply the custom language header
-        if(i_acceptLanguage != null)
-        {
-          header.addChildElement(createLanguageHeader());
-        }
+        // create the Security Header
+        SOAPElement security =
+                header.addChildElement("Security", "wsse", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd");
+        // create a UsernameToken element
+        SOAPElement usernameToken =
+                security.addChildElement("UsernameToken", "wsse");
+        usernameToken.addAttribute(new QName("xmlns:wsu"), "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd");
+        // add the username element
+        SOAPElement username =
+                usernameToken.addChildElement("Username", "wsse");
+        username.addTextNode(properties.getProperty("soap.Login"));
+        // add the password element
+        SOAPElement password =
+                usernameToken.addChildElement("Password", "wsse");
+        password.setAttribute("Type", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText");
+        password.addTextNode(properties.getProperty("soap.Password"));
       }
       catch (Exception e)
       {
@@ -144,68 +96,6 @@ public static final String SECURITY = "Security";
     // allow any other handler to execute
     return true;
   }
-
-  /**
-   * This method creates a custom language header, which allows the scoring service
-   * to use the given language if possible.
-   * @return A custom language header
-   * @throws Exception
-   */
-  private SOAPElement createLanguageHeader() throws Exception
-  {
-    SOAPFactory factory = SOAPFactory.newInstance();
-
-    // create a custom language header
-    SOAPElement languageHeader =
-            factory.createElement(CLIENT_ACCEPT_LANGUAGE_HEADER,SPSS_PREFIX,SPSS_HEADER_NAMESPACE);
-
-    // include the language text
-    languageHeader.addTextNode(i_acceptLanguage);
-
-    return languageHeader;
-  }
-
-  /**
-   * Creates the WS-Security SOAP header for UsernameToken as SOAPElement.
-   *
-   * @return the WS-Security SOAP header for UsernameToken
-   * @throws Exception as appropriate
-   */
-  private SOAPElement createUsernameTokenSecurityHeader() throws Exception
-  {
-    SOAPFactory factory = SOAPFactory.newInstance();
-
-    // create a UsernameToken element
-    SOAPElement usernameToken =
-            factory.createElement(USERNAME_TOKEN, WSSE_PREFIX, WS_SECURITY_NAMESPACE2);
-
-    // add the username element
-    SOAPElement usernameElement =
-            factory.createElement(USERNAME, WSSE_PREFIX, WS_SECURITY_NAMESPACE);
-    usernameElement.addTextNode(properties.getProperty("soap.Login"));
-    usernameToken.addChildElement(usernameElement);
-
-    // add the password element
-    SOAPElement passwordElement =
-            factory.createElement(PASSWORD, WSSE_PREFIX, WS_SECURITY_NAMESPACE3);
-    passwordElement.addTextNode(properties.getProperty("soap.Password"));
-    usernameToken.addChildElement(passwordElement);
-
-    // create the Security Header
-    SOAPElement securityHeader =
-            factory.createElement(SECURITY, WSSE_PREFIX, WS_SECURITY_NAMESPACE);
-    securityHeader.addChildElement(usernameToken);
-
-    return securityHeader;
-  }
-
-
-
-
-
-
-
-
 
   private synchronized IDBankCustomersPortType getCustomersPort() {
     if (servicePort == null) {
